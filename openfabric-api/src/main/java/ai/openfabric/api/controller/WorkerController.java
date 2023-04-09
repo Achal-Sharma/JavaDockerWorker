274c0e347f589e8ca534bdefdf76da636361b563
Achal Sharma <44108410+Achal-Sharma@users.noreply.github.com>
2023-04-09 16:25:19 +0100

    Instantiating client config and http client in constructor

--- openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
+++ openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
@@ -1,16 +1,38 @@
 package ai.openfabric.api.controller;
 
+import ai.openfabric.api.repository.WorkerRepository;
+import com.github.dockerjava.api.*;
+import com.github.dockerjava.core.*;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import java.time.Duration;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${node.api.path}/worker")
-public class WorkerController {
+public class WorkerController
+{
+    private DockerClientConfig config;
+    private DockerClient dockerClient;
+    private DockerHttpClient httpClient;
+    private WorkerRepository workerRepository;
 
-
-    @PostMapping(path = "/hello")
-    public @ResponseBody String hello(@RequestBody String name) {
-        return "Hello!" + name;
+    public WorkerController(WorkerRepository repository)
+    {
+        this.config = DefaultDockerClientConfig.createDefaultConfigBuilder().build();
+        this.httpClient = createDockerHttpClient(config);
+        this.dockerClient = DockerClientImpl.getInstance(config, httpClient);
+        this.workerRepository = repository;
     }
 
-
+    private DockerHttpClient createDockerHttpClient(DockerClientConfig config)
+    {
+        return new ApacheDockerHttpClient.Builder()
+                .dockerHost(config.getDockerHost())
+                .sslConfig(config.getSSLConfig())
+                .maxConnections(100)
+                .connectionTimeout(Duration.ofSeconds(30))
+                .responseTimeout(Duration.ofSeconds(45))
+                .build();
+    }
 }
